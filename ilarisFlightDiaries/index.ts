694a372d49948560d431976e61ca251e60d7b6f3
nicktvdd <nicktvdd@gmail.com>
2024-02-07 18:13:41 +0200

    9.10

--- ilarisFlightDiaries/index.ts
+++ ilarisFlightDiaries/index.ts
@@ -1,14 +1,27 @@
 import express from 'express';
+import data from './data/diagnoses';
 
 const app = express();
 app.use(express.json());
 
 const PORT = 3001;
 
+type Diagnosis = {
+  code: string;
+  name: string;
+  latin?: string;
+
+};
+
 app.get('/api/ping', (_req, res) => {
   res.set('Acces-Control-Allow-Origin', 'http://localhost:3000');
   console.log('someone pinged here');
   res.send('pong');
+});
+
+app.get('/api/diagnoses', (_req, res) => {
+  res.set('Acces-Control-Allow-Origin', 'http://localhost:3000');
+  res.send(data as Diagnosis[]);
 });
 
 app.listen(PORT, () => {
